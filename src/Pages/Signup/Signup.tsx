272c603c14b37e15aaa621737c9568a3034b9968
nadir.NACEF <107315189+77nadhir77@users.noreply.github.com>
2025-02-06 20:44:22 +0100

    signup added

--- src/Pages/Signup/Signup.tsx
+++ src/Pages/Signup/Signup.tsx
@@ -1,104 +1,87 @@
-import React from 'react'
-import MaxWidthWrapper from '../../components/MaxWidthWrapper'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom'; // Ensure you have this import if you are using Link from react-router-dom
+import MaxWidthWrapper from '../../components/MaxWidthWrapper';
+import axios from 'axios';
+import { useUserContext } from '../../Context/UserProvider';
+import { jwtDecode } from 'jwt-decode';
+ // Adjust the import path according to your project structure
 
-const Signup:React.FC = () => {
+const Signup: React.FC = () => {
+  const navigate = useNavigate(); // Ensure you have this import if you are using useNavigate()
+  const {setAuthTokens, setUser} = useUserContext()
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const username = (document.getElementById('username') as HTMLInputElement)?.value;
+    const password = (document.getElementById('password1') as HTMLInputElement)?.value;
+    
+
+
+    // Assuming you have an API endpoint to handle signup
+    await axios.post('http://localhost:8000/signup', {
+        username: username,
+        password: password,
+      }
+    )
+    .then(async response => {
+      console.log(response);
+      await axios.post("http://localhost:8000/login", {
+          "username": username,
+          "password": password,
+      })
+      .then(response => {
+          setAuthTokens(response.data)
+          setUser(jwtDecode(response.data.accessToken))
+          localStorage.setItem('authTokens', JSON.stringify(response.data))
+      })
+      navigate('/')
+      })
+    .catch(error => {
+      console.log(error);
+    });
+  };
+
   return (
-   
     <MaxWidthWrapper className={'py-14 w-full'}>
-      <form>
-      <div className="space-y-12">
-        
-        <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
-          <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
+      <form onSubmit={handleSubmit}>
+        <div className="space-y-12">
+          <div className="border-b border-gray-900/10 pb-12">
+            <h2 className="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
 
-          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div className="sm:col-span-3">
-              <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">First name</label>
-              <div className="mt-2">
-                <input type="text" name="first-name" id="first-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-3">
+                <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">Username</label>
+                <div className="mt-2">
+                  <input type="text" name="username" id="username" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                </div>
               </div>
-            </div>
+              <div className="sm:col-span-4">
+                <label htmlFor="password1" className="block text-sm font-medium leading-6 text-gray-900">password</label>
+                <div className="mt-2">
+                  <input id="password1" name="password1" type="password" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                </div>
+              </div>
 
-            <div className="sm:col-span-3">
-              <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">Last name</label>
-              <div className="mt-2">
-                <input type="text" name="last-name" id="last-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              <div className="sm:col-span-4">
+                <label htmlFor="password2" className="block text-sm font-medium leading-6 text-gray-900">confirm password</label>
+                <div className="mt-2">
+                  <input name="password2" type="password" id="password2" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                </div>
               </div>
-            </div>
-
-            <div className="sm:col-span-4">
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-              <div className="mt-2">
-                <input id="email" name="email" type="email" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-              </div>
-            </div>
-            <div className="sm:col-span-4">
-              <label htmlFor="password1" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
-              <div className="mt-2">
-                <input name="password1" type="password" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-              </div>
-            </div>
-            <div className="sm:col-span-4">
-              <label htmlFor="password1" className="block text-sm font-medium leading-6 text-gray-900">Password confirmation</label>
-              <div className="mt-2">
-                <input name="password2" type="password" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-              </div>
-            </div>
-          
-            <div className="sm:col-span-3">
-              <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">Country</label>
-              <div className="mt-2">
-                <select id="country" name="country" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-                  <option>United States</option>
-                  <option>Canada</option>
-                  <option>Mexico</option>
-                </select>
-              </div>
-            </div>
-
-            <div className="col-span-full">
-              <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-900">Street address</label>
-              <div className="mt-2">
-                <input type="text" name="street-address" id="street-address" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-              </div>
-            </div>
-
-            <div className="sm:col-span-2 sm:col-start-1">
-              <label htmlFor="city" className="block text-sm font-medium leading-6 text-gray-900">City</label>
-              <div className="mt-2">
-                <input type="text" name="city" id="city" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
-              </div>
-            </div>
-
-            <div className="sm:col-span-2">
-              <label htmlFor="region" className="block text-sm font-medium leading-6 text-gray-900">State / Province</label>
-              <div className="mt-2">
-                <input type="text" name="region" id="region" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-              </div>
-            </div>
-
-            <div className="sm:col-span-2">
-              <label htmlFor="postal-code" className="block text-sm font-medium leading-6 text-gray-900">ZIP / Postal code</label>
-              <div className="mt-2">
-                <input type="text" name="postal-code" id="postal-code" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-              </div>
+  
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="mt-6 flex items-center justify-end gap-x-6">
-        <button type="reset" className="text-sm font-semibold leading-6 text-gray-900">Reset</button>
-        <button type="submit" className="rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-pr focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">Save</button>
-      </div>
-    </form>you have already an account? <Link to='/Login'>Login</Link>
-    <p></p>
-
+        <div className="mt-6 flex items-center justify-end gap-x-6">
+          <button type="reset" className="text-sm font-semibold leading-6 text-gray-900">Reset</button>
+          <button type="submit" className="rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-primary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">Save</button>
+        </div>
+      </form>
+      <p>you have already an account? <Link to='/Login'>Login</Link></p>
     </MaxWidthWrapper>
-   
-  )
-}
+  );
+};
 
-export default Signup
+
+export default Signup;
